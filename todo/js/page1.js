21b238991013467b4d93cfbd61bff1aea1021001
bxphxmxth <fushs@hotmail.fr>
2021-06-14 11:17:20 +0200

    d

--- todo/js/page1.js
+++ todo/js/page1.js
@@ -1,32 +1,23 @@
-// # Todo list 
+let title = document.createElement("h1");
+let titleText = document.createTextNode("My Todo-List");
+title.appendChild(titleText)
 
-// ### Outils
-// - Github. Faites un commit pour chaque fonctionnalité réalisée. Nom du repo : js_dom_todolist
+document.body.appendChild(title)
 
-// ### Design
-// - Vous trouverez un design de base dans le dossier 'exemple design'.
-// - Attention, n'allez pas plus loin que le design de base au risque de perdre beaucoup de temps. La priorité est de faire toutes les fonctionnalités avant la deadline. 
+let myInput = document.createElement("input");
+myInput.setAttribute("class", "todo-input");
+document.body.appendChild(myInput);
 
-// ### Consignes et fonctionnalités (voir les vidéos)
-// - Avoir au minimum 2 fichiers js (import & export)
-// - faire une zone de texte pour écrire une tâche. (input)
-// - Pour enregistrer une tâche, passer (2 possibilités) :
-//     - par un button add/ajouter
-//     - par la touche 'Enter' (event.key)
-// - sur la tâche en question : 
-//     - un button/checkbox qui valide les tâches terminées 
-//     - un button qui permet de modifier le nom de la tâche
-//     - un button qui supprime la tâche
-// - faire trois button pour filtrer les tâches :
-//     - all/tout : au clique, toutes les tâches s'affichent.
-//     - to do/à faire : au clique, les tâches non terminées s'affichent et les tâches terminées disparait
-//     - done/terminé : au clique, les tâches terminées s'affichent et les non terminées disparait
-// - faire un boutton "list clear" qui permet de supprimer toutes les tâches. Autrement dit , de nettoyer la liste 
+let btnSubmit = document.createElement("button");
+btnSubmit.setAttribute("class", "todo-button fas fa-plus-square");
+document.body.appendChild(btnSubmit);
 
-// ### Bonus fonctionnalités
-// - pour le mode "edit", faites en sorte de pouvoir modifier en double cliquant sur le nom de la tâche
-// - pour le mode "delete", faites en sorte de demander une confirmation avant de supprimer
-// - Rajoutez un mode "sombre" pour le design. (comme sur facebook)
-// - faire un "drap and drog" sur les tâches. (voir sur internet)
+let todoContainer = document.createElement("div");
+todoContainer.setAttribute("class", "todo-container");
+document.body.appendChild(todoContainer);
+console.log(todoContainer);
 
-
+let todoList = document.createElement("ul");
+todoList.setAttribute("class","todo-list");
+todoContainer.appendChild(todoList);
+console.log(todoList);
